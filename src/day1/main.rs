f967b3f4393301e617a1e7dbb80213b65c79d632
coljnr9 <coljnr9@hotmail.com>
2022-12-02 23:58:57 -0800

    iterators until the end

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, thread::park_timeout};
+use std::fs;
 
 fn load_input_file() -> String {
     fs::read_to_string("src/day1/input.txt").expect("Failed to read input file")
@@ -6,10 +6,10 @@ fn load_input_file() -> String {
 
 fn process_input_string(input: String) -> u32 {
     let strings = input.split("\n\n");
-    let strings: Vec<Vec<&str>> = strings.map(|s| s.split('\n').collect()).collect();
+    let strings = strings.map(|s| s.split('\n'));
 
     let mut max_sum = 0;
-    for (i, string) in strings.iter().enumerate() {
+    for string in strings {
         let mut elf_calories = 0;
         for s in string {
             match s.parse::<u32>() {
