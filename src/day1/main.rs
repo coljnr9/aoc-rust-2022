5d41beba8bd81cd0dfc72f9537665884382563c5
coljnr9 <coljnr9@hotmail.com>
2022-12-02 23:56:19 -0800

    Day1 - naive

---
+++ src/day1/main.rs
@@ -0,0 +1,31 @@
+use std::{fs, thread::park_timeout};
+
+fn load_input_file() -> String {
+    fs::read_to_string("src/day1/input.txt").expect("Failed to read input file")
+}
+
+fn process_input_string(input: String) -> u32 {
+    let strings = input.split("\n\n");
+    let strings: Vec<Vec<&str>> = strings.map(|s| s.split('\n').collect()).collect();
+
+    let mut max_sum = 0;
+    for (i, string) in strings.iter().enumerate() {
+        let mut elf_calories = 0;
+        for s in string {
+            match s.parse::<u32>() {
+                Ok(v) => elf_calories += v,
+                Err(_) => eprintln!("ruh roh"),
+            }
+        }
+        if elf_calories > max_sum {
+            max_sum = elf_calories;
+        }
+    }
+    max_sum
+}
+
+fn main() {
+    let input_str = load_input_file();
+    let best_elf = process_input_string(input_str);
+    println!("{:?}", best_elf);
+}
